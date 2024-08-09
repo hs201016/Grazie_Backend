71f0d7be2bcf42010ab7d24079214613f3c6bb7c
류희수 <hs201016@gmail.com>
2024-08-10 01:42:20 +0900

    Feat : 회원가입 구현
    비밀번호 암호화, ENUM (CUSTOMER)

---
+++ src/main/java/Grazie/com/Grazie_Backend/member/UserRepository.java
@@ -0,0 +1,15 @@
+package Grazie.com.Grazie_Backend.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+     Optional<User> findByUserId(String userid);
+
+
+
+}
