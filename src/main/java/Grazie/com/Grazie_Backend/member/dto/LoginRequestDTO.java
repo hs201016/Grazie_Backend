81b965eb5c38bf7f7794af10664acd7c29a8f1ed
류희수 <hs201016@gmail.com>
2024-10-11 00:31:33 +0900

    Feat : 회원가입시, 비밀번호 변경시에 검증하는 validation 추가하였음. 지금은 테스트 단계라 주석처리. 시현할 때 주석 혜제할 예정입니다

--- src/main/java/Grazie/com/Grazie_Backend/member/dto/LoginRequestDTO.java
+++ src/main/java/Grazie/com/Grazie_Backend/member/dto/LoginRequestDTO.java
@@ -1,5 +1,8 @@
 package Grazie.com.Grazie_Backend.member.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,11 +10,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class LoginRequestDTO {
-
+    @NotEmpty(message = "사용자 ID는 필수입니다.")
+//    @Size(min = 4, max = 20, message = "사용자 ID는 4자 이상, 20자 이하이어야 합니다.")
     private String userid;
+
+    @NotEmpty(message = "비밀번호는 필수입니다.")
+//    @Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$", message = "비밀번호는 영문자와 숫자를 모두 포함해야 합니다.")
     private String password;
-
-
-
 }
