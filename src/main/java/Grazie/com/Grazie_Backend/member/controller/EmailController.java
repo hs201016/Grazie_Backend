8f4079f3b8fe15b47df204085e8f5a117adb9b14
류희수 <hs201016@gmail.com>
2024-10-13 17:52:19 +0900

    Feat & FIX : 회원가입시 응답값 user객체 -> pk로 변경 & 아이디 찾기 이메일 검증로직, 비밀번호 찾기 멘트 수정

--- src/main/java/Grazie/com/Grazie_Backend/member/controller/EmailController.java
+++ src/main/java/Grazie/com/Grazie_Backend/member/controller/EmailController.java
@@ -1,16 +1,15 @@
 package Grazie.com.Grazie_Backend.member.controller;
 
 import Grazie.com.Grazie_Backend.Config.UserAdapter;
+import Grazie.com.Grazie_Backend.member.dto.FindIdRequest;
 import Grazie.com.Grazie_Backend.member.service.PasswordResetService;
 import Grazie.com.Grazie_Backend.member.dto.UserDTO;
+import Grazie.com.Grazie_Backend.member.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private final PasswordResetService passwordResetService;
+    private final UserService userService;
 
     @PostMapping("/request-temp-password")
     public ResponseEntity<?> requestTempPassword(@AuthenticationPrincipal UserAdapter userAdapter) {
@@ -28,6 +28,16 @@ public class EmailController {
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("임시 비밀번호 발급 실패");
+        }
+    }
+
+    @PostMapping("/find-id")
+    public ResponseEntity<String> findId(@RequestBody FindIdRequest request) {
+        try {
+            userService.findId(request.getEmail());
+            return ResponseEntity.ok("아이디가 해당 이메일로 발송되었습니다.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
